d7e83583f9b05e3e4cdea9528f6aa83b0fabe730
carlosdgt95 <cdgt9522@gmail.com>
2021-12-29 13:23:27 -0500

    peso cuantitova

--- Rproyecto.R
+++ Rproyecto.R
@@ -35,3 +35,50 @@ tablaf_ansiedad
 prop.table(tablaf_ansiedad)
 diagrama_sexo = barplot(tablaf_ansiedad , main="Diagrama Episodios Semanales de ansiedad" , xlab="Numero Episodios", ylab = 'Casos',names.arg = c("0-1","2-3","4-5","5+"),col="#2ECC71")
 #cuantitativas
+
+#peso
+#frecuencia
+peso=datos_estudiantes$Peso
+peso1=range(peso,na.rm=TRUE)
+amplitud=(peso1[2]-peso1[1])
+amplitud
+k=7
+ancho=amplitud/k
+
+tabla_peso=table.freq(hist(peso, breaks = seq(from=peso1[1],to=peso1[2],by=ancho), include.lowest = T, right = F, plot = F))
+names(tabla_peso)=c("Límite inferior de clase","Límite superior de clase","Marca de clase",
+                    "Frecuencia absoluta","Frecuencia relativa absoluta en %","Frecuencia acumulada",
+                    "Frecuencia acumulada relativa en %")
+tabla_peso
+
+#medias estadistica
+media=mean(is.na(peso))
+
+mediana=median(is.na(peso))
+
+desviacion=sd(is.na(peso))
+
+q1=quantile(peso,c(0.25),na.rm=TRUE)[1]
+q2=quantile(peso,c(0.50),na.rm=TRUE)[1]
+q3=quantile(peso,c(0.75),na.rm=TRUE)[1]
+curtosis=kurtosis(peso)
+sesgo=skewness(peso)
+medidas=data.frame(media,mediana,q1,q2,q3,curtosis,sesgo,desviacion,row.names = c("Medidas"))
+names(medidas)=c("Media","Mediana","Cuartil 1","Cuartil 2","Cuartil 3",
+                 "Curtosis","Sesgo","Desviación Estándar")
+medidas
+
+#desviacion
+hist(peso, breaks = seq(from=peso1[1],to=peso1[2],by=ancho), include.lowest = T, right = F,
+     main="Histograma de frecuencias de Edad",ylab="Frecuencia absoluta",xlab="Peso")
+#ojiva
+# Ojiva Peso
+q = c(0.05,0.12,0.55,0.75,0.95) ##datos para grafico
+quartil = quantile(datos_estudiantes$Peso,probs = c(0.05,0.12,0.55,0.75,0.95),na.rm = T)
+quartil
+
+plot(quartil,q, main = "Ojiva Peso", xlab = "Puntaje", ylab = "Frecuencia Relativa")
+lines(quartil,q,type="l", col="brown")
+
+#cajas de textos
+boxplot(peso,main="Diagrama de Peso",ylab="Peso / Libras")
