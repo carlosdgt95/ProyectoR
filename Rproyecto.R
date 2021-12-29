a585f75ac13ee62bca5246526c589b63c4fa333c
Luis Alejandro Donoso Bravo <74844225+GhostWarrior25@users.noreply.github.com>
2021-12-29 13:13:04 -0500

    Cambios limpieza en los datos de Excel y cambios en diagrama de barras

--- Rproyecto.R
+++ Rproyecto.R
@@ -1,6 +1,6 @@
 
 library(readxl)
-datos_estudiantes <- read_excel("C:/Users/carlos Gomez/Desktop/proyecto estaditica/datos_estudiantes.xlsx")
+datos_estudiantes <- read_excel("datos_estudiantes.xlsx")
 datos_estudiantes
 
 #cualitativas tablas
@@ -8,30 +8,30 @@ datos_estudiantes
 tablaf_sexo=table(datos_estudiantes$Sexo)
 tablaf_sexo
 prop.table(tablaf_sexo)
-diagrama_sexo = barplot(tablaf_sexo, main="Diagrama Sexo" , xlab="sexo",names.arg = c("M","F"),col="#69b3a2")
+diagrama_sexo = barplot(tablaf_sexo, main="Diagrama Sexo" , xlab="sexo",names.arg = c("M","F"),col=c( 'lightblue', 'pink'))
 
 #Episodios_por_semana_estres
 
 tablaf_estres=table(datos_estudiantes$Episodios_por_semana_estres)
 tablaf_estres
 prop.table(tablaf_estres)
-diagrama_sexo = barplot(tablaf_estres, main="Diagrama Estres" , xlab="Estres",names.arg = c("0","1","2","3","4"),col="#69b3a2")
+diagrama_sexo = barplot(tablaf_estres, main=" Diagrama Episodios Semanales de Estres" , xlab="Numero Episodios", ylab = 'Casos',names.arg = c("0-1","2-3","4-5","5+"),col=c( '#F9E79F', '#F4D03F', '#B7950B', '#7D6608' ))
 #Episodios_por_semana_ira_frustacion
 
 tablaf_ira=table(datos_estudiantes$Episodios_por_semana_ira_frustacion)
 tablaf_ira
 prop.table(tablaf_estres)
-diagrama_sexo = barplot(tablaf_ira, main="Diagrama Ira&Frustracion" , xlab="Ira&Frustracion",names.arg = c("0","1","2","3","4"),col="#69b3a2")
+diagrama_sexo = barplot(tablaf_ira, main="Diagrama Episodios Semanales de Ira & Frustracion" , xlab="Numero Episodios", ylab = 'Casos',names.arg = c("0-1","2-3","4-5","5+"),col="#F9EF28")
 
 #Episodios_por_semana_miedo
 tablaf_miedo=table(datos_estudiantes$Episodios_por_semana_miedo)
 tablaf_miedo
 prop.table(tablaf_miedo)
-diagrama_sexo = barplot(tablaf_miedo, main="Diagrama Miedo" , xlab="Miedo",names.arg = c("0","1","2","3"),col="#69b3a2")
+diagrama_sexo = barplot(tablaf_miedo, main="Diagrama Episodios Semanales de Miedo" , xlab="Numero Episodios", ylab = 'Casos',names.arg = c("0-1","2-3","4-5","5+"),col="#69b3a2")
 
 #Episodios_por_semana_ansiedad
 tablaf_ansiedad=table(datos_estudiantes$Episodios_por_semana_ansiedad)
 tablaf_ansiedad
 prop.table(tablaf_ansiedad)
-diagrama_sexo = barplot(tablaf_ansiedad , main="Diagrama ansiedad" , xlab="Ansiedad",names.arg = c("0","1","2","3","4"),col="#69b3a2")
+diagrama_sexo = barplot(tablaf_ansiedad , main="Diagrama Episodios Semanales de ansiedad" , xlab="Numero Episodios", ylab = 'Casos',names.arg = c("0-1","2-3","4-5","5+"),col="#2ECC71")
 #cuantitativas
